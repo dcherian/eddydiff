ab96689c98560225fd5476c4d739968472024d20
dcherian <deepak@cherian.net>
2020-08-21 13:41:00 -0600

    Setup config

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='eddydiff',
+      version='0.1',
+      description='eddy diffusivity analysis',
+      url='http://github.com/dcherian/eddydiff',
+      author='Deepak Cherian',
+      author_email='deepak@cherian.net',
+      license='GPL3',
+      packages=['eddydiff'],
+      zip_safe=False)
