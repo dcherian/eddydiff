8bfb7b0dea4654e12069ed5f26511af92087e558
Deepak Cherian <deepak@cherian.net>
2021-12-10 16:48:31 -0700

    NATRE data is gridded in pressure so use that

--- eddydiff/natre.py
+++ eddydiff/natre.py
@@ -11,19 +11,26 @@ from . import sections
 def preprocess_natre(ds):
     def _regrid(ds):
         start = 10
-        step = 0.4
-        rounded = np.arange(start, start + step * 7962, step)
-        return ds.groupby_bins(
-            "DEPTH", rounded, labels=0.5 * (rounded[:-1] + rounded[1:])
+        step = 0.5
+        # There is one 3000m profile...
+        rounded = np.arange(start, 3100, step)
+        # pressure is on a regular 0.5dbar grid
+        regridded = (
+            ds.dropna("DEPTH")
+            .swap_dims({"DEPTH": "PRESSURE"})
+            .reindex(PRESSURE=rounded, method="nearest", tolerance=0.1)
         )
-
-    ds.load()
-
-    count = _regrid(ds["CHI-T"]).count()
-    if not (count.dropna("DEPTH_bins") < 2).all():
-        raise ValueError(
-            f"Regridding may be wrong for file: {ds.encoding['source'].split('/')[-1]}"
+        assert (
+            regridded.EPSILON.count().compute().data
+            == ds.EPSILON.count().compute().data
         )
+        return regridded
+
+        # return ds.groupby_bins(
+        #    "DEPTH", rounded, labels=0.5 * (rounded[:-1] + rounded[1:])
+        # )
+
+    # ds.load()
 
     ds["LATITUDE"] = np.round(ds.LATITUDE.data, 1)
     ds["LONGITUDE"] = np.round(ds.LONGITUDE.data, 1)
@@ -31,12 +38,20 @@ def preprocess_natre(ds):
     if ds.LONGITUDE.data > -26.9:
         ds["LONGITUDE"] = -26.8
 
+    regridded = _regrid(ds)
+
+    # count = _regrid(ds["CHI-T"]).count()
+    # if not (count.dropna("DEPTH_bins") < 2).all():
+    #    raise ValueError(
+    #        f"Regridding may be wrong for file: {ds.encoding['source'].split('/')[-1]}"
+    #    )
+    # regridded = _regrid(ds).first().rename({"DEPTH_bins": "depth"})
+    # regridded = ds.drop_vars("DEPTH").rename({"DEPTH": "idepth"})
+    # regridded["depth"] = ("idepth", ds.DEPTH.data)
+
     return (
-        _regrid(ds)
-        .first()
-        .rename(
+        regridded.rename(
             {
-                "DEPTH_bins": "depth",
                 "LATITUDE": "latitude",
                 "LONGITUDE": "longitude",
                 "CHI-T": "chi",
@@ -45,6 +60,7 @@ def preprocess_natre(ds):
                 "PSAL": "salt",
                 "PRESSURE": "pres",
                 "TIME": "time",
+                "DEPTH": "depth",
             }
         )
         .squeeze()
@@ -81,10 +97,11 @@ def combine_natre_files():
 
 
 def read_natre():
-    natre = xr.open_dataset(
-        "../datasets/natre_large_scale.nc", chunks={"latitude": 5, "longitude": 5}
-    )
+    # natre = xr.open_dataset(
+    #    "../datasets/natre_large_scale.nc", chunks={"latitude": 5, "longitude": 5}
+    # )
     # natre = natre.where(natre.chi.notnull() & natre.eps.notnull())
+    natre = combine_natre_files()
     natre = natre.set_coords(["time", "pres"])
 
     natre = natre.cf.guess_coord_axis()
@@ -95,10 +112,12 @@ def read_natre():
 
     if "neutral_density" not in natre.cf:
         natre["gamma_n"] = dcpy.oceans.neutral_density(natre)
-    natre = dcpy.oceans.thorpesort(natre, natre.gamma_n.cf.interpolate_na("Z"))
+    natre = dcpy.oceans.thorpesort(
+        natre, natre.gamma_n.drop("depth").interpolate_na("pres")
+    )
 
     natre = sections.add_ancillary_variables(natre, pref=1000)
-    # natre = natre.where(natre.chi > 1e-14)
+    natre = natre.where(natre.chi > 1e-14)
 
     # messes up cf-xarray
     if "depth" in natre.time.dims:
