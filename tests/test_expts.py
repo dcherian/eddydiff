edf552b465299806f2e55a43514ec37be1016d5b
dcherian <deepak@cherian.net>
2022-05-18 13:56:34 -0600

    Use threads in test.

--- tests/test_expts.py
+++ tests/test_expts.py
@@ -3,7 +3,6 @@
 import os
 
 import cf_xarray as cfxr
-import distributed
 import numpy as np
 import pandas as pd
 
@@ -36,31 +35,21 @@ def test_natre():
 
     os.chdir("./notebooks/")
 
-    with distributed.Client(
-        n_workers=1,
-        threads_per_worker=4,
-        env={"OMP_NUM_THREADS": 1, "NUMBA_NUM_THREADS": 1, "MKL_NUM_THREADS": 1},
-    ) as client:
-
-        natre = read_natre(load=True)
-        bins = ed.sections.choose_bins(
-            natre.gamma_n, depth_range=np.arange(150, 2001, 100)
+    natre = read_natre(load=True)
+    bins = ed.sections.choose_bins(natre.gamma_n, depth_range=np.arange(150, 2001, 100))
+    with raise_if_dask_computes():
+        actual = ed.sections.bin_average_vertical(
+            natre.cf.stack(
+                {"cast": ("latitude", "longitude")}, create_index=False
+            ).assign_coords(cast=("cast", np.arange(100), {"cf_role": "profile_id"})),
+            "neutral_density",
+            bins,
+            blocksize=20,
         )
-        with raise_if_dask_computes():
-            actual = ed.sections.bin_average_vertical(
-                natre.cf.stack(
-                    {"cast": ("latitude", "longitude")}, create_index=False
-                ).assign_coords(
-                    cast=("cast", np.arange(100), {"cf_role": "profile_id"})
-                ),
-                "neutral_density",
-                bins,
-                blocksize=20,
-            )
-        actual.load(client=client)
-        actual.to_netcdf("../tests/estimates/natre-test-run.nc")
-        actual.attrs.pop("commit", None)
+    actual.load(client="threads")
+    actual.to_netcdf("../tests/estimates/natre-test-run.nc")
+    actual.attrs.pop("commit", None)
 
-        expected = xr.load_dataset("../tests/estimates/natre.nc")
-        expected.attrs.pop("commit", None)
-        assert_allclose(expected, actual, atol=0, rtol=1e-2)
+    expected = xr.load_dataset("../tests/estimates/natre.nc")
+    expected.attrs.pop("commit", None)
+    assert_allclose(expected, actual, atol=0, rtol=1e-2)
