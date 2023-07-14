c9fa4b8941a16bc5dba6d082ab6f8763e581ca26
Deepak Cherian <dcherian@users.noreply.github.com>
2023-07-14 09:02:20 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="eddydiff",
-    version="0.1",
+    version="0.2",
     description="eddy diffusivity analysis",
     url="http://github.com/dcherian/eddydiff",
     author="Deepak Cherian",
